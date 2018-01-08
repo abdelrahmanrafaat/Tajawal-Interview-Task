a78aae6921120fda2c496478d811c5c018c72e0f
abdelrahmanrafaat <abdel.rahmanrafaat5@gmail.com>
2018-01-08 21:44:52 +0200

    Added HotelsFinderCommand and command entery point

---
+++ src/Commands/HotelsFinderCommand.php
@@ -0,0 +1,27 @@
+<?php
+namespace Tajawal\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Question\Question;
+
+class HotelsFinderCommand extends Command
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+            
+    }
+
+}
