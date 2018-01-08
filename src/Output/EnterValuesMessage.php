c4f20bc65d4283f0c1fa6dc42c0382a921f85869
abdelrahmanrafaat <abdel.rahmanrafaat5@gmail.com>
2018-01-09 01:31:48 +0200

    Added EnterValuesMessage which abstract the Strategy for displaying Messages for entering Name,Price,city,availability

---
+++ src/Output/EnterValuesMessage.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tajawal\Output;
+
+use Tajawal\Output\EnterPriceMessage;
+use Tajawal\Output\EnterCityMessage;
+use Tajawal\Output\EnterNameMessage;
+use Tajawal\Output\EnterAvailabilityMessage;
+
+class EnterValuesMessage{
+
+    protected $outputWriter;
+
+    public function __construct($outputWriter){
+        $this->outputWriter = $outputWriter;
+    }
+
+    public function for($key){
+        if($key == 'name')
+            return new EnterNameMessage($this->outputWriter);
+        
+        if($key == 'city')
+            return new EnterCityMessage($this->outputWriter);
+
+        if($key == 'price')
+            return new EnterPriceMessage($this->outputWriter);
+        
+        if($key == 'availability')
+            return new EnterAvailabilityMessage($this->outputWriter);
+    }
+}
+
+                
