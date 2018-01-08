94073e1cff571cf95abd68ecb75005b51d008847
abdelrahmanrafaat <abdel.rahmanrafaat5@gmail.com>
2018-01-09 00:43:34 +0200

    Added Date Helpers and formaters using Carbon Date library

---
+++ src/Helpers/CarbonHelpers.php
@@ -0,0 +1,23 @@
+<?php
+namespace Tajawal\Helpers;
+
+use Carbon\Carbon;
+
+class CarbonHelpers{
+
+    public static function convertFromString($stringDate){
+        return Carbon::createFromFormat('d-m-Y', $stringDate)->startOfDay();
+    }
+
+    public static function getMinAndMax($firstDate, $secoundDate){
+        return [
+            'min' => $firstDate->min($secoundDate),
+            'max' => $firstDate->max($secoundDate),
+        ];
+    }
+
+    public static function greaterThanOrEqual($first, $secound){
+        return $first->gte($secound);
+    }
+
+}
