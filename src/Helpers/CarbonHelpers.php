2d4046de20110e9d2c0c2d4a868494e264095fc2
abdelrahmanrafaat <abdel.rahmanrafaat5@gmail.com>
2018-01-10 17:04:53 +0200

    Adding PhpUnit Configuration

--- src/Helpers/CarbonHelpers.php
+++ src/Helpers/CarbonHelpers.php
@@ -8,14 +8,14 @@ class CarbonHelpers{
         return Carbon::createFromFormat('d-m-Y', $stringDate)->startOfDay();
     }
 
-    public static function getMinAndMax($firstDate, $secoundDate){
+    public static function getMinAndMax(Carbon $firstDate, Carbon $secoundDate){
         return [
             'min' => $firstDate->min($secoundDate),
             'max' => $firstDate->max($secoundDate),
         ];
     }
 
-    public static function greaterThanOrEqual($first, $secound){
+    public static function greaterThanOrEqual(Carbon $first, Carbon $secound){
         return $first->gte($secound);
     }
 
