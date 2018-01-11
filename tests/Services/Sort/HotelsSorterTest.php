98b740b0242c8cea25215661229d7016ea0493a8
abdelrahmanrafaat <abdel.rahmanrafaat5@gmail.com>
2018-01-11 16:37:26 +0200

    sorting hotels test

--- tests/Services/Sort/HotelsSorterTest.php
+++ tests/Services/Sort/HotelsSorterTest.php
@@ -3,8 +3,54 @@
 namespace Tests\Services\Sort;
 
 use PHPUnit\Framework\TestCase;
+use Tajawal\Services\Sort\HotelsSorter;
 
 class HotelsSorterTest extends TestCase
 {
-    
+    private $hotelsSorter;
+
+    public function __construct(){
+        $this->hotelsSorter = new HotelsSorter;
+
+        parent::__construct();
+    }
+
+    public function testSortByName(){
+        $unsortedHotels = [
+            (object)[
+                'name' => 'c-hotel'
+            ],
+            (object)[
+                'name' => 'a-hotel'
+            ],
+            (object)[
+                'name' => 'b-hotel'
+            ],
+        ];
+
+        $sortedHotels = $this->hotelsSorter->sort($unsortedHotels, 'name');
+
+        $this->assertEquals($sortedHotels[0]->name, 'a-hotel');
+        $this->assertEquals($sortedHotels[2]->name, 'c-hotel');
+    }
+
+    public function testSortByPrice(){
+        $unsortedHotels = [
+            (object)[
+                'price' => 10
+            ],
+            (object)[
+                'price' => 20
+            ],
+            (object)[
+                'price' => 7
+            ],
+        ];
+
+        $sortedHotels = $this->hotelsSorter->sort($unsortedHotels, 'price');
+
+        $this->assertEquals($sortedHotels[0]->price, 7);
+        $this->assertEquals($sortedHotels[2]->price, 20);
+    }
+
 }
